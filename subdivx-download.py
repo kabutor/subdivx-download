0c7c6d2f44ee2b4f5cc2065fb7b4c6f3838a3b34
Martín Gaitán <gaitan@gmail.com>
2012-02-28 23:44:48 -0300

    unpack rared subtitles with external tool unrar

--- subdivx-download.py
+++ subdivx-download.py
@@ -3,7 +3,7 @@
 import argparse
 import logging
 import logging.handlers
-import os.path
+import os
 import urllib
 import urllib2
 
@@ -22,7 +22,7 @@ LOGGER_LEVEL = logging.DEBUG
 LOGGER_FORMATTER = logging.Formatter('%(asctime)-25s %(levelname)-8s %(name)-29s %(message)s', '%Y-%m-%d %H:%M:%S')
 
 class NoResultsError(Exception):
-    pass    
+    pass
 
 
 def is_rarfile(fn):
@@ -32,9 +32,9 @@ def is_rarfile(fn):
 
 def setup_logger(level):
     global logger
-    
+
     logger = logging.getLogger(__file__[:-3])
-    
+
     logfile = logging.handlers.RotatingFileHandler(logger.name+'.log', maxBytes=1000 * 1024, backupCount=9)
     logfile.setFormatter(LOGGER_FORMATTER)
     logger.addHandler(logfile)
@@ -51,13 +51,13 @@ def get_subtitle_url(series_name, series_id, series_quality):
     soup = BeautifulSoup(page)
 
     results_descriptions = soup('div', id='buscador_detalle_sub')
-    
+
     if not results_descriptions:
         raise(NoResultsError(' '.join(['No suitable subtitles were found for:',
                                       series_name,
                                       series_id,
                                       series_quality])))
-        
+
     search_match = '%s %s %s' % (series_name, series_id, series_quality)
     matcher = SequenceMatcher(lambda x: x==" " or x==".", search_match)
 
@@ -65,7 +65,7 @@ def get_subtitle_url(series_name, series_id, series_quality):
         matcher.set_seq2(seq)
 
         blocks = matcher.get_matching_blocks()
-        
+
         scores = []
         for block in blocks:
             long_start   = block[1] - block[0]
@@ -76,24 +76,24 @@ def get_subtitle_url(series_name, series_id, series_quality):
             r = m2.ratio()
             if r > .995: return 100
             else: scores.append(r)
-    
+
         return int(100 * max(scores))
-    
+
     best_match = [calculate_ratio(''.join([e for e in description.recursiveChildGenerator() if isinstance(e,unicode)]))
                   for description in results_descriptions]
-    
+
     best_match_index = best_match.index(max(best_match))
-    
+
     return results_descriptions[best_match_index].nextSibling.find(**SUBDIVX_DOWNLOAD_MATCHER)['href']
-    
+
 def get_subtitle(url, path):
     in_data = urllib2.urlopen(url)
     temp_file = NamedTemporaryFile()
-    
+
     temp_file.write(in_data.read())
     in_data.close()
     temp_file.seek(0)
-    
+
     if is_zipfile(temp_file.name):
         zip_file = ZipFile(temp_file)
         for name in zip_file.namelist():
@@ -104,13 +104,26 @@ def get_subtitle(url, path):
 
         zip_file.close()
     elif is_rarfile(temp_file.name):
-        logger.info(' '.join(['Saving rared subtitle as', path + '.rar']))
-        out_file = open(path + '.rar', 'w')
-        out_file.write(temp_file.read())
-        temp_file.close()
-        out_file.close()
+        rar_path = path + '.rar'
+        logger.info('Saving rared subtitle as %s' % rar_path)
+        with open(rar_path, 'w') as out_file:
+            out_file.write(temp_file.read())
 
-    
+        try:
+            import subprocess
+            #extract all .srt in the rared file
+            ret_code = subprocess.call(['unrar', 'e', '-n*srt', rar_path])
+            if ret_code == 0:
+                logger.info('Unpacking rared subtitle to %s' % os.path.dirname(path))
+                os.remove(rar_path)
+        except OSError:
+            logger.info('Unpacking rared subtitle failed.'
+                        'Please, install unrar to automate this step.')
+
+
+    temp_file.close()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str, help="The series episode identifier to be downloaded")
@@ -122,7 +135,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     setup_logger(LOGGER_LEVEL)
-    
+
     if not args.quiet:
         console = logging.StreamHandler()
         console.setFormatter(LOGGER_FORMATTER)
@@ -133,7 +146,7 @@ if __name__ == '__main__':
     except NoResultsError, e:
         logger.error(e.message)
         raise
-    
+
     out_file_name = '/%s %s %s' % (args.series_name, args.series_id, args.series_quality)
-    
+
     get_subtitle(url, os.path.abspath(args.path) + out_file_name)
